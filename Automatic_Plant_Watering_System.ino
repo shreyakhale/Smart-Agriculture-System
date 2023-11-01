b3364d45abad6eed280e05ad34e3d8283b58ecd9
Shreya Khale <shreyakhale2002@gmail.com>
2023-11-01 15:36:30 +0530

    Update Automatic_Plant_Watering_System.ino

--- Automatic_Plant_Watering_System.ino
+++ Automatic_Plant_Watering_System.ino
@@ -1,133 +1,93 @@
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+
+#include <ArduinoJson.h>
+
 // Sensor pins
 #define sensorPin_rain 8
 #define relay_pin A0
-const int sensor_pin = A1;	/* Soil moisture sensor O/P pin */
-const int sensor_pin_mq2 = A2;	/* MQ2 sensor O/P pin */
+const int sensor_pin = A1;     /* Soil moisture sensor O/P pin */
+const int sensor_pin_mq2 = A2; /* MQ2 sensor O/P pin */
 
-//the time we give the sensor to calibrate (10-60 secs according to the datasheet)
-int calibrationTime = 30;        
+// Define a JSON buffer
+StaticJsonDocument<200> jsonBuffer;
 
-//the time when the sensor outputs a low impulse
-long unsigned int lowIn;         
+// The time we give the sensor to calibrate (10-60 secs according to the datasheet)
+int calibrationTime = 5;
 
-//the amount of milliseconds the sensor has to be low 
-//before we assume all motion has stopped
-long unsigned int pause = 5000;  
+// The time when the sensor outputs a low impulse
+long unsigned int lowIn;
+
+// The amount of milliseconds the sensor has to be low
+// before we assume all motion has stopped
+long unsigned int pause = 5000;
 
 boolean lockLow = true;
-boolean takeLowTime;  
+boolean takeLowTime;
 
-int pirPin = 3;    //the digital pin connected to the PIR sensor's output
+int pirPin = 3;    // The digital pin connected to the PIR sensor's output
 int ledPin = 13;
 
-
 void setup() {
-  
-	Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(relay_pin, OUTPUT);
   pinMode(pirPin, INPUT);
   pinMode(ledPin, OUTPUT);
-  //digitalWrite(pirPin, LOW);
 
-  //give the sensor some time to calibrate
+  // Give the sensor some time to calibrate
   Serial.print("calibrating sensor ");
-    for(int i = 0; i < calibrationTime; i++){
-      Serial.print(".");
-      delay(1000);
-      }
-    Serial.println(" done");
-    Serial.println("SENSOR ACTIVE");
-    //digitalWrite(relay_pin, LOW);
-    delay(50);
-   
+  for (int i = 0; i < calibrationTime; i++) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println(" done");
+  Serial.println("SENSOR ACTIVE");
+  delay(50);
 }
 
 void loop() {
-	//get the reading from the function below and print it
-	int val_rain = digitalRead(sensorPin_rain);  
+  // Get the reading from the function below and store it in a JSON object
+  jsonBuffer["rain"] = digitalRead(sensorPin_rain);
+  int sensor_analog = analogRead(sensor_pin);
+  float moisture_percentage = (100 - (sensor_analog / 1023.00) * 100);
+  jsonBuffer["moisture"] = moisture_percentage;
+  jsonBuffer["relay"] = moisture_percentage < 20 && digitalRead(sensorPin_rain);
+  jsonBuffer["pir"] = digitalRead(pirPin);
 
-  //Rain
-	Serial.println("Rain: ");
-	Serial.println(val_rain);
-  if (val_rain) {
-		Serial.println("Status: Clear");
-	} else {
-		Serial.println("Status: It's raining");
-	}
-  
+  unsigned int sensorValue = analogRead(sensor_pin_mq2);
+  unsigned int outputValue = map(sensorValue, 0, 1023, 0, 255);
+  jsonBuffer["gas"] = outputValue;
 
-  //Moisture
-  float moisture_percentage;
-  int sensor_analog;
-  sensor_analog = analogRead(sensor_pin);
-  moisture_percentage = ( 100 - ( (sensor_analog/1023.00) * 100 ) );
-  Serial.print("Moisture Percentage = ");
-  Serial.print(moisture_percentage);
-  Serial.print("%\n\n");
+  // Serialize the JSON object and send it over the serial port
+  serializeJson(jsonBuffer, Serial);
+  Serial.println();
 
-  //Logic for relay
-  if (moisture_percentage < 30){
-    if (val_rain == 1) {
-      digitalWrite(relay_pin, HIGH);
-      Serial.println("Relay turned ON - Motor ON");
+  delay(8000);
+  if (digitalRead(pirPin) == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    if (lockLow) {
+      lockLow = false;
+      Serial.println("---");
+      Serial.print("motion detected at ");
+      Serial.print(millis() / 1000);
+      Serial.println(" sec");
+      delay(50);
     }
-    else{
-      digitalWrite(relay_pin, LOW);
-    }       
+    takeLowTime = true;
   }
-  else{
-  digitalWrite(relay_pin, LOW);
+
+  if (digitalRead(pirPin) == LOW) {
+    digitalWrite(ledPin, LOW);
+    if (takeLowTime) {
+      lowIn = millis();
+      takeLowTime = false;
+    }
+    if (!lockLow && millis() - lowIn > pause) {
+      lockLow = true;
+      Serial.print("motion ended at ");
+      Serial.print((millis() - pause) / 1000);
+      Serial.println(" sec");
+      delay(50);
+    }
   }
-    
-  //Gas
-  Serial.print("Analog output: ");
-  Serial.println(readSensor());
-    
-	delay(8000);	// Take a reading every second
-	Serial.println();
-  if(digitalRead(pirPin) == HIGH){
-       digitalWrite(ledPin, HIGH);   //the led visualizes the sensors output pin state
-       if(lockLow){  
-         //makes sure we wait for a transition to LOW before any further output is made:
-         lockLow = false;            
-         Serial.println("---");
-         Serial.print("motion detected at ");
-         Serial.print(millis()/1000);
-         Serial.println(" sec"); 
-         delay(50);
-         }         
-         takeLowTime = true;
-       }
-
-  if(digitalRead(pirPin) == LOW){       
-    digitalWrite(ledPin, LOW);  //the led visualizes the sensors output pin state
-
-    if(takeLowTime){
-    lowIn = millis();          //save the time of the transition from high to LOW
-    takeLowTime = false;       //make sure this is only done at the start of a LOW phase
-    }
-    //if the sensor is low for more than the given pause, 
-    //we assume that no more motion is going to happen
-    if(!lockLow && millis() - lowIn > pause){  
-        //makes sure this block of code is only executed again after 
-        //a new motion sequence has been detected
-        lockLow = true;                        
-        Serial.print("motion ended at ");      //output
-        Serial.print((millis() - pause)/1000);
-        Serial.println(" sec");
-        delay(50);
-        }
-    }
 }
-
-//  This function returns the analog data to calling function
-int readSensor() {
-  unsigned int sensorValue = analogRead(sensor_pin_mq2);  // Read the analog value from sensor
-  unsigned int outputValue = map(sensorValue, 0, 1023, 0, 255); // map the 10-bit data to 8-bit data
-  if (outputValue > 650) //make this 200 for gas
-    Serial.println("Gas detected");
-  else
-    Serial.println("No gas");
-  return outputValue;             // Return analog moisture value
-}
-
